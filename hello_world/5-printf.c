a710e27e3e09d779f181bf1dbb5a8c5d2fc399ee
Hanene07 <arrahanane@hotmail.com>
2023-02-17 01:55:26 -0800

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#inclure <stdio.h>
+
+/**
+ * principal - Point d'entée
+ *
+ * Retour toujours 0 (Succès)
+ */
+
+int principal ( void )
+{
+	printf ( "with proper grammar, but the outcome is a piece of art, \n " );
+	retour ( 0 );
+}
