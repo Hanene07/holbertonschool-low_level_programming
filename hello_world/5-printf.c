38a9b5c4f9ab70bd1556ea499f495ed42f863b8e
Hanene07 <arrahanane@hotmail.com>
2023-02-17 02:15:26 -0800

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
-# inclure  < stdio.h >
+#include <stdio.h>
 
-/* *
- * * principal - Point d'entrée
- * *
- * * Retour : Toujours 0 (Succès)
- *  */
-int  principal ( vide )
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
-		printf ( " avec une grammaire correcte, mais le résultat est une œuvre d'art, \n " );
-			retour ( 0 );
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
