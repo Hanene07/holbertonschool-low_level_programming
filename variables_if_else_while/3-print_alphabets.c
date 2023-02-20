713b3351604a2c3134736e3a6525f20890cd96c1
Hanene07 <arrahanane@hotmail.com>
2023-02-20 07:35:01 -0800

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,13 @@ int main(void)
 {
 	char c;
 
-	for (c = 'A'; c <= 'Z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{ 
 		putchar(c);
+
+	for ( 'A'; c <= 'Z'; c++)
+
+		putchar(c);	
 	}
 	putchar('\n');
 	return (0);
