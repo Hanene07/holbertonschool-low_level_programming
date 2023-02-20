6bd67835ac5de22549494a101f476b7df23b74e6
Hanene07 <arrahanane@hotmail.com>
2023-02-20 01:49:27 -0800

    adding 0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <heure.h>
+
+/* plus d'en-têtes y vont */
+
+/* main - Entry point */
+
+int main (void)
+{
+
+int n ;
+
+srand(temps(0));
+n = rand() - RAND_MAX / 2 ;
+
+{
+/* votre code y va */
+printf("if 0>=n %d\n" );
+printf("if n<=0 %d\n" ); 	
+return	(0);
+}
