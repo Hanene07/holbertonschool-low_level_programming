1a6c8f544c9207b81619fc5c06bc1b2c416abef8
Hanene07 <arrahanane@hotmail.com>
2023-02-21 02:27:45 -0800

    adding 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Succes)
+*/
+
+int main(void)
+
+{
+	int a = 0;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
