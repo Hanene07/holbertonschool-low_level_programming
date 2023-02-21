ca5d8c1365690cef50f3177ea7f20ddfe3786a4b
Hanene07 <arrahanane@hotmail.com>
2023-02-21 10:28:14 -0800

    adding 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Succes)
- */
+* main - Entry point
+* Return: Always 0 (Succes)
+*/
 
 int main(void)
 
 {
 	int a = 0;
+
 	for (a = 0; a <= 9; a++)
 	{
 		printf("%d", a);
