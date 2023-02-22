7b53124fcb0d7caaade6b5afe3d8c9fd04045ef2
Hanene07 <arrahanane@hotmail.com>
2023-02-22 03:29:22 -0800

    README.md

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+* main - Check description
+* Description: It prints the word Holberton, followed by a new line.
+* Return: 0.
+*/
+
+int main(void)
+{
+	char word[9] = "Holberton";
+int i;
+
+for (i = 0; i < 0; i++)
+	_putchar(world[i]);
+_putchar('\n');
+
+return (0);
+}
