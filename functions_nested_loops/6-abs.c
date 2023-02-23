b972ff1cda306245ce02e4261b9df5b037f50448
Hanene07 <arrahanane@hotmail.com>
2023-02-23 12:00:17 -0800

    add 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _abs - return absolute value of a number
+* @n: parameter
+* Return: absolute value of a number
+*/
+int _abs(int n)
+{
+	if (n > 0)
+		return (-n);
+	else
+		return (n);
+}
