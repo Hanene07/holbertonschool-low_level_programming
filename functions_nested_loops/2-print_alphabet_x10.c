fee24217408acf281978a91c43a54c60151bfb3d
Hanene07 <arrahanane@hotmail.com>
2023-02-23 03:49:31 -0800

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - make the alphabet_x1
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int i = 0;
+
+	while (i <= 9)
+	{
+
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	}
+	_putchar('\n');
+	i++;
+}
