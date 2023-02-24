d0ce90d2d8299c4ba90c386e92c7f56f632ab04e
Hanene07 <arrahanane@hotmail.com>
2023-02-24 05:33:30 -0800

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,7 +3,8 @@
 
 /**
 * mul - a function that multiplies two integers
-* @c: parameter of the function
+* @a: parameter of the function
+* @b: parameter of the function
 * Return: the result of the computer
 */
 int mul(int a, int b)
