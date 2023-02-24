e74d9b5d4c505be43e1f6c0773ceed402538495c
Hanene07 <arrahanane@hotmail.com>
2023-02-24 08:01:21 -0800

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 	int i;
 	int n;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (n = 0; i <= 14; n++)
+		for (n = 0; n <= 14; n++)
 		{
 			if (n > 9)
 			{
