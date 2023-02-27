afd9d1291400d2b4f540d6d5402110e4de2834fe
Hanene07 <arrahanane@hotmail.com>
2023-02-27 11:15:44 -0800

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * puts2 - a function that prints every other character of a string
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
  * @str: An input string
  */
 void puts2(char *str)
