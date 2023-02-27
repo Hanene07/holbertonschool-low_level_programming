ffd822e9819ec61b106d3c46fcb59beab38812be
Hanene07 <arrahanane@hotmail.com>
2023-02-27 07:28:33 -0800

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,17 +10,17 @@ void print_rev(char *s)
 	int a, b, len;
 
 	a = 0;
-	
+
 	while (s[a] != '\0')
 	{
 		a++;
 	}
-	
-	len = i;
-	
-	for (b = len - 1; b >= 0, j--)
+
+	len = a;
+
+	for (b = len - 1; b >= 0; b--)
 	{
-		_putchar(s[j]);
+		_putchar(s[b]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
